9175398e24c72bd2f30a43da61cfc05c578eaaed
Ingbox <ingbox01@gmail.com>
2022-08-12 04:50:59 +0900

    apple_clone_0812_caption

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -105,6 +105,9 @@
             rect1X: [0, 0, {start: 0, end: 0}],
             rect2X: [0, 0, {start: 0, end: 0}],
             blendHeight: [0, 0, {start: 0, end: 0}],
+            canvas_scale: [0, 0, {start: 0, end: 0}],
+            canvasCaption_opacity : [0, 1, {start: 0, end: 0}],
+            canvasCaption_translateY: [20, 0, {start: 0, end: 0}],
             rectStartY: 0
         }
     }];
@@ -135,6 +138,16 @@
 
     setCanvasImages();
 
+    // Menu Sticky 처리
+    function checkMenu() {
+        console.log(yOffset);
+        if (yOffset > 44) {
+            document.body.classList.add('local-nav-sticky');
+        } else {
+            document.body.classList.remove('local-nav-sticky');
+        }
+    }
+
     function setLayout() {
         for(let i = 0; i < sceneInfo.length; i++){
             if (sceneInfo[i].type ==='sticky'){
@@ -159,6 +172,7 @@
         const heightRatio = window.innerHeight / 1080;
         sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
         sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+        // sceneInfo[3].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
     }
 
     function calcValues(values, currentYOffset) {
@@ -221,7 +235,6 @@
                 } else {
                     objs.messageB.style.opacity = calcValues(values.messageB_opacity_out, currentYOffset);
                     objs.messageB.style.transform = `translate3d(0, ${calcValues(values.messageB_translateY_out, currentYOffset)}%, 0)`;
-                    // objs.messageB.style.color = '#D3D3D3';
                 }
 
                 if (scrollRatio <= 0.62){
@@ -291,8 +304,7 @@
                     const widthRatio = window.innerWidth / objs.canvas.width;
                     const heightRatio = window.innerHeight / objs.canvas.height;
                     let canvasScaleRatio;
-                    
-  
+
                     if (widthRatio <= heightRatio){
                       canvasScaleRatio = heightRatio;
                     } else {
@@ -323,7 +335,6 @@
                   const widthRatio = window.innerWidth / objs.canvas.width;
                   const heightRatio = window.innerHeight / objs.canvas.height;
                   let canvasScaleRatio;
-                  
 
                   if (widthRatio <= heightRatio){
                     canvasScaleRatio = heightRatio;
@@ -364,10 +375,10 @@
                   console.log(values.rect1X[2].end);
 
                   if (scrollRatio < values.rect1X[2].end) { // end의 시점은 canvas의 Top이 딱 맞았을 때 보다 scrollRatio가 작으면 canvas 이전 상태
-                    step = 1;
+                    step = 1; // 시점 1
                     objs.canvas.classList.remove('sticky');
                   } else {
-                    step = 2;
+                    step = 2; // 시점 2
                     // 이미지 블랜드 실행
 
                     values.blendHeight[0] = 0;
@@ -378,15 +389,37 @@
 
                     objs.context.drawImage(objs.images[1], 0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
                                                             0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight);
-                    
                     objs.canvas.classList.add('sticky');
                     objs.canvas.style.top= `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
+
+                    if (scrollRatio > values.blendHeight[2].end) {
+                        values.canvas_scale[0] = canvasScaleRatio; // 이미 scale 처리를 해줬었음
+                        values.canvas_scale[1] = document.body.offsetWidth / (1.5 * objs.canvas.width);// Scale 최종 값
+                        values.canvas_scale[2].start = values.blendHeight[2].end;
+                        values.canvas_scale[2].end = values.canvas_scale[2].start + 0.2;
+
+                        objs.canvas.style.transform = `scale(${calcValues(values.canvas_scale, currentYOffset)})`;
+                        objs.canvas.style.marginTop = 0;
+                    }
+
+                    if (scrollRatio > values.canvas_scale[2].end && values.canvas_scale[2].end > 0) { // 초기값이 0이니까 current_Scene 바로 시작 방지
+                        objs.canvas.classList.remove('sticky');
+                        objs.canvas.style.marginTop = `${scrollHeight * 0.4}px`;
+
+                        values.canvasCaption_opacity[2].start = values.canvas_scale[2].end;
+                        values.canvasCaption_opacity[2].end = values.canvasCaption_opacity[2].start + 0.1;
+                        objs.canvasCaption.style.opacity = calcValues(values.canvasCaption_opacity, currentYOffset);
+
+                        values.canvasCaption_translateY[2].start = values.canvas_scale[2].end;
+                        values.canvasCaption_translateY[2].end = values.canvasCaption_translateY[2].start + 0.1;
+                        objs.canvasCaption.style.transform = `translate3d(0, ${calcValues(values.canvasCaption_translateY, currentYOffset)}%, 0)`;
+                    }
+
                   }
                 break;
         }
     }
 
-   
 
     function scrollLoop(){
         enterNewScene = false;
@@ -421,11 +454,13 @@
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
     });
     window.addEventListener('resize', setLayout);
+
+    //Scroll 시 체크
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
         scrollLoop();
+        checkMenu();
     });
-    
 
     setLayout();
 })();
